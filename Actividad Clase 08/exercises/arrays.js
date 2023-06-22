95f99fd0cff5e8940df039af3f5a41cdbae94e40
Joaquin Fernan Canto <joaquinfr.canto@gmail.com>
2023-06-22 16:26:28 -0300

    Terminado hasta if_else.

--- Actividad Clase 08/exercises/arrays.js
+++ Actividad Clase 08/exercises/arrays.js
@@ -10,9 +10,30 @@ var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Ago
 meses.sort();
 console.log(meses);
 
-/* C -- Agregar un elemento al principio y al final del array (utilizar unshift y push).
-d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
-e. Invertir el orden del array (utilizar reverse).
-f. Unir todos los elementos del array en un único string donde cada mes este
-separado por un guión - (utilizar join).
-g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
+/* C -- Agregar un elemento al principio y al final del array (utilizar unshift y push).*/
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+meses.unshift("Diciembre");
+meses.push("Enero");
+
+/* D -- Quitar un elemento del principio y del final del array (utilizar shift y pop).*/
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+meses.shift();
+meses.pop();
+
+/* E -- Invertir el orden del array (utilizar reverse).*/
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+meses.reverse();
+
+/* F -- Unir todos los elementos del array en un único string donde cada mes este separado por un guión (utilizar join).*/
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+meses.join("-");
+
+/* G -- Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).*/
+
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var meses2;
+meses2 = meses.slice(4,11);
